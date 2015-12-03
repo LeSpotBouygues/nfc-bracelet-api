a33f94209232ed01998117a3da6b0236c062cbf6
nana <nana@192.168.0.15>
2015-12-03 01:23:23 +0100

    WS-11.0.1 <nana@macbook-air-de-eric.local Overwrite local to /Users/nana/Desktop/nfc-bracelet-api

--- app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by superphung on 11/27/15.
- */
-var koa = require('koa');
-var mongoose = require('mongoose');
-
-module.exports = koa();
-mongoose.connect('mongodb://localhost/nfc-api-db');
--- index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by superphung on 11/28/15.
- */
-var app = require('./app');
-
-require('./routes/companion');
-
-app.listen(process.env.PORT || 3000);
